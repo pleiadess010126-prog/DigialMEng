f80e8cda1b369dc77ae660b9eb32ce55e2bca5da
deepakdee02 <106740797+a1saraf@users.noreply.github.com>
2025-12-28 14:15:12 +0530

    Fix: Add s3-request-presigner package and webpack externals for AWS SDK

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,19 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '2mb',
     },
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@aws-sdk/client-s3': 'commonjs @aws-sdk/client-s3',
+        '@aws-sdk/s3-request-presigner': 'commonjs @aws-sdk/s3-request-presigner',
+        '@aws-sdk/client-dynamodb': 'commonjs @aws-sdk/client-dynamodb',
+        '@aws-sdk/lib-dynamodb': 'commonjs @aws-sdk/lib-dynamodb',
+        '@aws-sdk/client-cognito-identity-provider': 'commonjs @aws-sdk/client-cognito-identity-provider',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
